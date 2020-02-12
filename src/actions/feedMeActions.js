d85a0ccb7d945a0652b3078d667f4ace4376bd42
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-02-12 14:33:04 -0800

    add feedme actions

---
+++ src/actions/feedMeActions.js
@@ -0,0 +1,21 @@
+export const ADD_COFFEE = 'ADD_COFFEE';
+export const addCoffee = () => ({
+  type: ADD_COFFEE
+});
+
+export const ADD_SNACK = 'ADD_SNACK';
+export const addSnack = () => ({
+  type: ADD_SNACK
+});
+
+
+export const ADD_NAP = 'ADD_NAP';
+export const addNap = () => ({
+  type: ADD_NAP
+});
+
+
+export const ADD_STUDY = 'ADD_STUDY';
+export const addStudy = () => ({
+  type: ADD_STUDY
+});
